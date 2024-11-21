aca09ad30643051af628cb22719fe22f51e802ca
Burak <burakyapici747@gmail.com>
2024-11-21 22:23:05 +0300

    feat: implement CustomUserDetails class with complete UserDetails contract

--- src/main/java/com/user_messaging_system/core_library/response/CustomUserDetails.java
+++ src/main/java/com/user_messaging_system/core_library/response/CustomUserDetails.java
@@ -21,7 +21,6 @@ public class CustomUserDetails implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return this.authorities;
     }
-
     @Override
     public String getPassword() {
         return this.password;
@@ -34,22 +33,22 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 
     public String getId(){
